7e2f006ce97a68e629d5dac68330d06ecf816750
James <james@redkite.com.ph>
2019-02-21 18:49:02 +0800

    Finish Pages: Contact Us, About Us, Homepage

--- functions.php
+++ functions.php
@@ -58,9 +58,14 @@ class StarterSite extends Timber\Site {
 	}
 
 	public function register_shortcodes() {
-		add_shortcode( 'primary_button', 'primary_button_shortcode' );
+		add_shortcode( 'button', 'button_shortcode' );
 
-		function primary_button_shortcode( $atts ) {
+		function button_shortcode( $atts ) {
+			if (isset($atts['id'])) {
+				$id = esc_attr($atts['id']);
+			} else {
+				$id = "btn-blue";
+			}
 			if (isset( $atts['link'] )) {
 				$link = esc_url_raw( $atts['link']);
 			} else {
@@ -71,9 +76,11 @@ class StarterSite extends Timber\Site {
 			}
 			if (isset( $atts['class'] )) {
 				$class = esc_textarea($atts['class']);
+			} else {
+				$class = '';
 			}
 
-			return Timber::compile( 'shortcodes/primary_button.twig', array( 'link' => $link, 'text' => $text, 'class' => $class ) );
+			return Timber::compile( 'shortcodes/buttons.twig', array( 'id' => $id, 'link' => $link, 'text' => $text, 'class' => $class ) );
 		}
 	}
 
